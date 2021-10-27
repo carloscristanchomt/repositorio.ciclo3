9b3a11ec30ed8993b5923077e7a845d3273acf4b
carloscristanchomt <91539087+carloscristanchomt@users.noreply.github.com>
2021-10-27 13:44:55 -0500

    reto 5

---
+++ Ciclo3Rentcar/src/test/java/Ciclo3Rentcar/Ciclo3Rentcar/Ciclo3RentcarApplicationTests.java
@@ -0,0 +1,13 @@
+package Ciclo3Rentcar.Ciclo3Rentcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ciclo3RentcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
